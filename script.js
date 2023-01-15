6054950bc235f14278e63f4e6ec6e13a91b5c575
eSuardOdin <esuardodin@gmail.com>
2023-01-15 02:02:18 +0100

    Trying to get icon clic to work in order to toggle Book.isRead

--- script.js
+++ script.js
@@ -19,11 +19,28 @@ Book.prototype.getInfo = function()
 
 Book.prototype.toggleRead = function() {
     this.isRead = (!this.isRead);
-} 
+}
 
-const dune = new Book('Dune', 'Frank Herbert', 894);
-const hobbit = new Book('The Hobbit', 'J.R.R Tolkien', 368);
-const bigBrother = new Book ('1984', 'George Orwell', 396);
+Book.prototype.getHTML = function(index) {
+    const icon = this.isRead ? './book-check.svg' : './book-clock-outline.svg';
+    const res = 
+    `
+    <div class="book-container">
+        <p class="book-title">${this.title}</p>
+        <p class="book-author">${this.author}</p>
+        <p class="book-pages">${this.pages} pages</p>
+        <div class="book-icons">
+            <object class="book-read-toggle" id="${index}" data="${icon}" width="30" height="30"></object>
+            <object data="./book-minus-outline.svg" width="30" height="30"></object>
+        </div>
+    </div> 
+    `;
+    return res;
+}
+
+const dune = new Book('Dune', 'Frank Herbert', 894, true);
+const hobbit = new Book('The Hobbit', 'J.R.R Tolkien', 368, true);
+const bigBrother = new Book ('1984', 'George Orwell', 396, true);
 const lostTime = new Book ('In Search of Lost Time', 'Marcel Proust', 468);
 const warAndPeace = new Book ('War and Peace', 'Leon Tolstoï', 1296);
 
@@ -36,3 +53,31 @@ const warAndPeace = new Book ('War and Peace', 'Leon Tolstoï', 1296);
 // console.log(book);
 // console.log(book.getInfo());
 console.log(myLibrary);
+
+// -------------------------------------------
+// ------------- DOM -------------------------
+// -------------------------------------------
+const body = document.querySelector('.body');
+
+
+
+
+
+
+
+myLibrary.forEach((el, index) => {
+    const icon = el.isRead ? './book-check.svg' : './book-clock-outline.svg';
+    body.insertAdjacentHTML('beforeend', el.getHTML(index));
+});
+
+const icons = document.querySelectorAll('.book-read-toggle');
+const books = document.querySelectorAll('.book-container');
+icons.forEach((el) => {
+    el.addEventListener('click', () => console.log('hello'));
+})
+
+books.forEach(el => {
+    el.addEventListener('click', () => console.log('hello2'));
+})
+
+// <object data="./icons/calendar.svg" width="30" height="30"></object>
